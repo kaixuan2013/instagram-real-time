2a09a0087869af1b522a8ef02ba8b2b39f7308d1
Kai Xuan <kaixuan.ng.2013@sis.smu.edu.sg>
2015-03-06 14:11:45 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ var hashTag = 'test';
 
 //app.set('views', __dirname + '/views');
 //app.engine('html', require('ejs').renderFile);
-//app.set('view engine', 'jade');
+app.set('view engine', 'jade');
+
+app.get('/album.html', function(req, res){
+  hashTag = req.query.name;
+  res.render('album');
+});
 
 /**
  * Set the paths for your files
@@ -99,9 +104,8 @@ app.configure(function(){
 /**
  * Render your index/view "my choice was not use jade"
  */
-app.get('/views', function(req, res){
-  hashTag = req.query.name;
-  res.render('album');
+app.get("/views", function(req, res){
+    res.render("album");
 });
 
 // check subscriptions
